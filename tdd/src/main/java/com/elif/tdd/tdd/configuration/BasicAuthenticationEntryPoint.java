7bcb7dbf25939bbdf4b6f30074802059941fe308
elif akgun <elif.akgun13@gmail.com>
2020-02-01 19:59:53 +0300

    some bugs..

--- tdd/src/main/java/com/elif/tdd/tdd/configuration/BasicAuthenticationEntryPoint.java
+++ tdd/src/main/java/com/elif/tdd/tdd/configuration/BasicAuthenticationEntryPoint.java
@@ -15,7 +15,8 @@ public class BasicAuthenticationEntryPoint implements AuthenticationEntryPoint{
 	@Override
 	public void commence(HttpServletRequest request, HttpServletResponse response,
 			AuthenticationException authException) throws IOException, ServletException {
-			response.sendError(HttpStatus.UNAUTHORIZED.value(), HttpStatus.UNAUTHORIZED.getReasonPhrase());
+			
+		response.sendError(HttpStatus.UNAUTHORIZED.value(), HttpStatus.UNAUTHORIZED.getReasonPhrase());
 	}
 
 }
